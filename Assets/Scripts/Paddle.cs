ab7dce5581f9d7bff73d0c4f15b0d64c67073120
Rahul Patwari <rahulpatwari233@gmail.com>
2019-04-04 20:26:10 +0530

    Make paddle follow mouse x position.
    Udemy: s05-53

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	[SerializeField] float screenWidthUnits = 16f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float mousePos = Input.mousePosition.x / Screen.width * screenWidthUnits;
+		Vector2 paddlePos = new Vector2 (mousePos, transform.position.y);
+
+		transform.position = paddlePos;
+	}
+}
